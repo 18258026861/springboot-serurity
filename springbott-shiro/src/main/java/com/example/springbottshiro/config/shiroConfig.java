38fe85d4a25f4143e146b541dda11730dc95d0a9
顽疾 <1061603811@qq.com>
2020-05-10 22:24:53 +0800

    shiro ok

--- springbott-shiro/src/main/java/com/example/springbottshiro/config/shiroConfig.java
+++ springbott-shiro/src/main/java/com/example/springbottshiro/config/shiroConfig.java
@@ -1,5 +1,6 @@
 package com.example.springbottshiro.config;
 
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,17 +24,17 @@ public class shiroConfig  {
         bean.setSecurityManager(defaultWebSecurityManager);
 
             /*添加shiro的内置过滤器
-            *  anon：无需认证
+            * anon：无需认证
             * authc ： 必须认证才能访问
             * user：必须拥有  记住我  功能才能访问
             * perms: 拥有对某个资源的权限才能访问
             * role： 拥有某个角色权限才能访问
-            *
             * */
         HashMap<String,String> map = new HashMap<>();
         /*设置资源权限
         * 这里的资源写的是路径（controller里的），不是页面*/
-//                  perms[user:add]代表用户登录并且拥有add权限  ，未授权为401，一般会跳转到未授权页面
+
+//             perms[user:add]代表用户登录并且拥有add权限  ，未授权为401，一般会跳转到未授权页面
         map.put("/toadd","perms[user:add]");
         map.put("/toupdate","perms[user:update]");
 //          将设置的资源权限放入过滤器
@@ -52,17 +53,21 @@ public class shiroConfig  {
     @Bean(name="manager")
     public DefaultWebSecurityManager defaultWebSecurityManager(@Qualifier("realm") realm realm){
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
-//          关联realm   ,这里的参数不能直接realm（），因为realm是spring管理的，所以在方法的参数上用spring容器的bean对象
+//          关联realm  ，使用spring容器参数
         defaultWebSecurityManager.setRealm(realm);
-
         return defaultWebSecurityManager;
     }
 
-//    realm  对象，需要自定义（创建一个realm类）   这是第一步第一步
+//    realm  对象，需要自定义（创建一个realm类）   这是第一步
     @Bean
     public realm realm(){
         return new realm();
     }
 
 
+//   整合shiroDialect： 用来整合shiro 和thymeleaf   这个用于使用shiro自定义tag
+    @Bean(name = "shiroDialect")
+    public ShiroDialect shiroDialect(){
+        return new ShiroDialect();
+    }
 }
